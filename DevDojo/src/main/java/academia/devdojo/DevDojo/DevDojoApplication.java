3680f505a5d9bc4763d2dbe190fe3b343711c9a1
murissi <lu.murissi@gmail.com>
2023-03-07 19:21:18 -0300

    primeiros passos

---
+++ DevDojo/src/main/java/academia/devdojo/DevDojo/DevDojoApplication.java
@@ -0,0 +1,13 @@
+package academia.devdojo.DevDojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevDojoApplication.class, args);
+	}
+
+}
