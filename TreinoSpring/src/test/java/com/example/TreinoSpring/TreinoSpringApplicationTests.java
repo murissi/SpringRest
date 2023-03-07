3cbb187577f1b9dcb9816b00bf45e46b566da35b
murissi <lu.murissi@gmail.com>
2023-03-06 22:49:04 -0300

    estudo spring

---
+++ TreinoSpring/src/test/java/com/example/TreinoSpring/TreinoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TreinoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
